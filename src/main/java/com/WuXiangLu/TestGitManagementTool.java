2e8a10182af35bba9b616a3df7102a68ba16d521
wuxianglu <2658936100@qq.com>
2022-08-16 14:48:49 +0800

    测试在IDEA中集成分布式版本控制工具Git，第二次提交的代码，添加了在控制台打印Hello World的语句。

--- src/main/java/com/WuXiangLu/TestGitManagementTool.java
+++ src/main/java/com/WuXiangLu/TestGitManagementTool.java
@@ -3,5 +3,6 @@ package com.WuXiangLu;
 public class TestGitManagementTool {
     public static void main(String[] args) {
         System.out.println("测试在IDEA中集成Git");
+        System.out.println("Hello World");
     }
 }
