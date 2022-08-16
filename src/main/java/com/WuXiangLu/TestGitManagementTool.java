61aa6dac46f35ed1c5dfc227ea7fe85462e96a0c
wuxianglu <2658936100@qq.com>
2022-08-16 15:26:26 +0800

    测试在IDEA中集成分布式版本控制工具Git，在master分支中添加一行在控制台打印输出“测试将本地工作区修改的文件 push(推送) 到对应github上的远程库”的语句。

--- src/main/java/com/WuXiangLu/TestGitManagementTool.java
+++ src/main/java/com/WuXiangLu/TestGitManagementTool.java
@@ -8,5 +8,6 @@ public class TestGitManagementTool {
         System.out.println("这是hot-fix分支中的内容");
         System.out.println("我是master分支，本行代码用于测试与hot-fix分支冲突合并的情况");
         System.out.println("我是hot-fix分支，本行代码用于测试分支冲突合并的情况");
+        System.out.println("测试将本地工作区修改的文件 push(推送) 到对应github上的远程库");
     }
 }
