166ef6a2c359fb9b93458217bc601b1e294079b0
wuxianglu <2658936100@qq.com>
2022-08-16 14:50:11 +0800

    测试在IDEA中集成分布式版本控制工具Git，第三次提交的代码，添加了在控制台打印“小吴学习Git”的语句。

--- src/main/java/com/WuXiangLu/TestGitManagementTool.java
+++ src/main/java/com/WuXiangLu/TestGitManagementTool.java
@@ -4,5 +4,6 @@ public class TestGitManagementTool {
     public static void main(String[] args) {
         System.out.println("测试在IDEA中集成Git");
         System.out.println("Hello World");
+        System.out.println("小吴学习Git");
     }
 }
